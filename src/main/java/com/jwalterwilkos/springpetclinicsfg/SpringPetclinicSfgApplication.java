1769362067ee4ec781be66c62e27b4d5d54d1d9d
John Wilkos <johnwilkosmail@gmail.com>
2021-06-16 14:58:22 +0200

    Initial commit

---
+++ src/main/java/com/jwalterwilkos/springpetclinicsfg/SpringPetclinicSfgApplication.java
@@ -0,0 +1,13 @@
+package com.jwalterwilkos.springpetclinicsfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetclinicSfgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetclinicSfgApplication.class, args);
+    }
+
+}
