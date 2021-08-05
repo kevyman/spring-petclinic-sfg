3d0de544a70400ff8f4d40d0386aaddf457c212e
John Wilkos <johnwilkosmail@gmail.com>
2021-08-05 17:32:15 +0200

    Refactored to common crud Interface. Closes #10

--- pet-clinic-data/src/main/java/com/jwalterwilkos/springpetclinicsfg/services/VetService.java
+++ pet-clinic-data/src/main/java/com/jwalterwilkos/springpetclinicsfg/services/VetService.java
@@ -1,14 +1,9 @@
 package com.jwalterwilkos.springpetclinicsfg.services;
 
-import com.jwalterwilkos.springpetclinicsfg.model.Owner;
 import com.jwalterwilkos.springpetclinicsfg.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
 
-    Vet save(Vet vet);
+public interface VetService extends CrudService<Vet, Long>{
 
-    Set<Vet> findAll();
 }
